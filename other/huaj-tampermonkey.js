63a0935df58ccfbbb4363ed6c03a0919697d96c9
blackmiaool <blackmiaool@gmail.com>
2017-05-04 23:15:00 +0800

    update other

--- other/huaj-tampermonkey.js
+++ other/huaj-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         huajiao
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       You
 // @match        http://www.huajiao.com/user/*
@@ -31,15 +31,161 @@
             let num = cnt / sum * 100;
             let text = cnt / sum * 100 + "";
             text = text.slice(0, 4) + "%";
+            $li.addClass("gotten");
+            $li.data("sum", sum);
             $li.append(`<div style="position:absolute;left:81px;bottom:-16px;">${text}</div>`)
                 .append(`<div data-miao="process-wrap" style="position:absolute;right:10px;width:110px;bottom:-12px;height:10px;background-color:rgba(240, 173, 78,0.3)" title="忠诚度">
-<div data-miao="process" style="position:absolute;left:0;top:0;bottom:0;background-color:rgb(240, 173, 78);width:${num}%">
+<div data-miao="process" style="position:absolute;left:0;top:0;bottom:0;background-color:rgb(240, 173, 78);width:${num>100?100:num}%">
 </div>
 </div>`).append($level);
         });
     }
+    const fansCache = []
+
+    function getFans(uid, target, cb) {
+        const key = uid + '-' + target;
+        if (fansCache[key] !== undefined) {
+            cb(fansCache[key]);
+            return;
+        }
+
+        function get(offset) {
+            $.ajax({
+                url: "http://webh.huajiao.com/fansrank",
+                jsonp: "_callback",
+                dataType: "jsonp",
+                data: {
+                    fmt: "jsonp",
+                    uid,
+                    offset,
+                },
+                success: function (list) {
+                    const usr = list.data.find(v => v.uid == target);
+                    if (usr) {
+                        fansCache[key] = usr;
+                        const index = list.data.indexOf(usr);
+                        cb(usr, index);
+                    } else if (offset < 45) {
+                        get(offset + 15);
+                    } else {
+                        fansCache[key] = false;
+                        cb();
+                    }
+                }
+            });
+        }
+        get(0);
+
+    }
+
+    function hover() {
+        const cache = {};
+        $("#user-list").on("mouseleave", "li", function () {
+            const $dom = $(this);
+            $dom.find(".m-anchor-panel").remove();
+        });
+        $("#user-list").on("mouseenter", "li.gotten", function () {
+            const $dom = $(this);
+
+            const uid = $dom.find("a").eq(0).attr("href").match(/\d+$/)[0];
+            const sum = $dom.data("sum");
+
+            const prev = $dom.find(".m-anchor-panel");
+            setTimeout(function () {
+                prev.remove();
+            });
+            const $panel = $(`<div class="m-anchor-panel" style="position:absolute;right:${$("#user-list").width()-20}px;height:130px;min-width:50px;background-color:white;white-space: nowrap;z-index:1;"></div>`);
+            $dom.append($panel);
+
+
+            if (cache[uid]) {
+                onData(cache[uid]);
+            } else {
+                $.post("//localhost:15420/getFollow", {
+                    uid
+                }, onData);
+            }
+
+
+            function onData(list) {
+                cache[uid] = list;
+
+
+
+                //list=list.slice(0,20);
+                let finishCnt = 0;
+
+                function finish() {
+                    $panel.append(`<div style="display: inline-block;vertical-align: top;">End</div>`);
+                }
+
+                function handle(anchor, cb) {
+                    const $anchor = $(`<div class="m-anchor" style="width:100px;height:100%;text-align:center;display:inline-block;position:relative;">
+<a href="http://www.huajiao.com/user/${anchor.uid}"><img src="${anchor.avatar}" style="width:80px;height:80px;display:inline-block;border-radius:40px;"></a>
+
+</div>`);
+
+                    getFans(anchor.uid, uid, function (usr, index) {
+                        finishCnt++;
+
+                        if (!usr) {
+                            if (finishCnt === list.length) {
+                                finish();
+                            }
+                            cb();
+                            return;
+                        }
+                        let inserted = false;
+                        $panel.find(".m-anchor").each(function () {
+                            if (inserted) {
+                                return;
+                            }
+                            const $dom = $(this);
+                            if ($dom.data("score") * 1 > usr.score) {
+                                inserted = true;
+                                $dom.before($anchor);
+                            }
+                        });
+                        if (!inserted) {
+                            $panel.append($anchor);
+                        }
+
+                        $anchor.data("score", usr.score);
+                        $anchor.append(`<div>${usr.score}</div>`)
+                            .append(`<div data-miao="process-wrap" style="margin-top:-1px;position:relative;margin-left:10%;margin-right:10%;width:80%;height:10px;background-color:rgba(240, 173, 78,0.3)" title="忠诚度">
+<div data-miao="process" style="position:absolute;left:0;top:0;bottom:0;background-color:rgb(240, 173, 78);width:${usr.score/sum*100}%">
+</div>`).append(`<div>榜${index+1}</div>`);
+                        if (finishCnt === list.length) {
+                            finish();
+                        }
+                        cb();
+                    });
+                }
+                if (!list.length) {
+                    return;
+                }
+                let index = 0;
+
+                function doIt() {
+                    setTimeout(function () {
+                        handle(list[index++], function () {
+                            if (index < list.length) {
+                                doIt();
+                            }
+                        });
+                    }, 50);
+
+                }
+                doIt();
+            }
+
+        });
+    }
 
     function main() {
+
+        hover();
+
         $(".user-list .info").each(function () {
             const $dom = $(this);
             handle($dom);
@@ -76,53 +222,7 @@
         saveConfig();
         const anchorId = $(".user_id").text().match(/\d+$/)[0];
 
-        !(function () { //bookmark
-            const checkState = miaoconfig.bookmark[anchorId] ? "checked" : "";
-            const $bookmark = $(`<label style="float:right;">订阅首页位置<input type="checkbox" name="bookmark-home" ${checkState}/></label>`);
-            $("#fansGiftRank").prev().append($bookmark);
-            $bookmark.find("input").on("change", function (e) {
-                const value = $(e.target).is(":checked");
-                miaoconfig.bookmark[anchorId] = value;
-                saveConfig();
-                if (value) {
-                    Notification.requestPermission();
-                }
-            });
 
-            setInterval(update, 10000);
-            let prePos = "";
-
-            function onPos(text) {
-                if (text === prePos) {
-                    return;
-                }
-                prePos = text;
-                if (miaoconfig.bookmark[anchorId]) {
-                    new Notification(text);
-                }
-            }
-            update();
-
-            function update(cb) {
-                $.get("http://www.huajiao.com/mobile/index", function (data) {
-                    var match = data.match(/CATEGORY_LIST_OPTIONS[\s\S]+?};/)[0].match(/uid":"\d+"/g);
-                    var pos;
-                    match.some(function (str, i) {
-                        str = str.match(/\d+/)[0]
-                        if (str == anchorId) {
-                            pos = i + 1;
-                            return true;
-                        }
-                    });
-                    //recordPos(pos);
-                    if (pos !== undefined) {
-                        onPos("首页位置" + pos);
-                    } else {
-                        onPos("没在首页");
-                    }
-                })
-            }
-        })();
 
 
     }
