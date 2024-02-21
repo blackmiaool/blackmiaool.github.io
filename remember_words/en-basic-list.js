93e20ff73b066b17e1711a3decf24b47cf457721
Tdndoki <152980972+Tdndoki@users.noreply.github.com>
2024-02-21 19:53:25 +0800

    update words

--- remember_words/en-basic-list.js
+++ remember_words/en-basic-list.js
@@ -1,8 +1,66 @@
-var list=`abdomen abide ability abnormal aboard abolish abound abroad abrupt absence absent absolute absorb abstract absurd abundance abundant abuse academic academy accelerate accent accept acceptance access accessory accident accidental acclaim accommodate accommodation accompany accomplish 
+var list={};
+list.a=`abdomen abide ability abnormal aboard abolish abound abroad abrupt absence absent absolute absorb abstract absurd abundance abundant abuse academic academy accelerate accent accept acceptance access accessory accident accidental acclaim accommodate accommodation accompany accomplish 
 accord accordance accordingly accountant accumulate accuracy accurate accuse accustomed ache achieve acid acknowledge acquaint acquaintance acquire acquisition acre acrobat aCrOSS act actress actual acute adapt addict addition additional adequate adhere adjacent adjective adjoin adjust administer administration admire admission admit adolescent adopt adore adult advance advanced advantage advent 
 adventure adverb adverse advertise advice advisable advise advocate aerial aeroplane aesthetic affair affect affection affiliate affirm affluent afford afraid after afternoon afterward against agency agenda agent aggravate aggressive agitatc agony agreeable agreement agriculture ahead air-conditioning aircraft airline airport alami album alcohol allege alleviate alliance allocate allowance alongside aloud 
 alphabet alternate alternative altitude altogether aluminum amateur amaze ambassador ambiguous ambition ambitious ambulance amend amiable amount ample amplifier amplify amuse analogue analogy analyse analysis analytic ancestor anchor anc anecdote angel anger angle anguish ankle anniversary announce annoy annual anonymous another answer ant antenna anticipate antique anxiety anxious 
 artery articulate artificial artist artistic ascend ascertain ash ashamed ashore aside ask asleep aspect aspire assassinate assault assemble assembly assert assess asset assign assignment assimilate assist assistance assistant associate association assume assumption assurance assure astonish astronaut astronomy athlete atmosphere atom attach attain attempt attend attendance attendant attention attitude attorney attract attractive 
 attribute auction audience audio audit auditorium augment August aunt aural authentic author authority auto automatic automation autonomy autumn auxiliary avail available avenue average avert aviation avoid await awake award aware awe awkward axe axis 
-anybody anyhow anyone anything anyway anywhere apart apartment apologise apology appal apparatus apparent appeal appear appearance appendix appetite applaud applause apple appliance applicable application apply appoint appointment appraisal appreciate approach appropriate approval approve approximate April apt arbitrary arch architect architecture area argue argument arise arithmetic aM1 arilly around arOUSC airange array arrest arrival arrogant 
+anybody anyhow anyone anything anyway anywhere apart apartment apologise apology appal apparatus apparent appeal appear appearance appendix appetite applaud applause apple appliance applicable application apply appoint appointment appraisal appreciate approach appropriate approval approve approximate April apt arbitrary arch architect architecture area argue argument arise arithmetic aM1 arilly around airange array arrest arrival arrogant 
 `;
+list.b=`bachelor bacon bacterium badge badminton
+baggage bait balance balcony bald ballet balloon ballot bandage bang bankrupt banner banquet barber
+bare bargain bark barn barrel barren barrier basin basis basket
+batch bath bathe bay beam bean bearing beast belly bend benign beverage bewilder bias bibliography
+bid bin bind biography bitter bizarre blackmail blade blame blank blanket blast blaze bleak bleed
+blend bless blind block bloom blossom blouse blunder blunt blur blush boast boil bold bolt
+bond bone boost booth border bosom bounce bound boundary
+bowel boycott brace bracket brake branch brand brandy brass brave breach breadth
+breakdown breast breath breathe breed breeze bribe bride brief briefcase
+bright brilliant brim brisk brittle  broadcast brochure bronze brook broom brow
+bruise brutal bubble bucket bud budget buffet bulb bulk bull bullet bulletin
+bully bump bunch bundle burden bureau bureaucracy burglar burn
+burst bury butcher butter bypass`
+list.c=`cab cabbage cabin cabinet cable cafeteria cage calcium calculate calendar calorie
+camel candidate cannon canoe canteen canvas cap capable capacity cape capitalism capsule
+captain captive capture carbohydrate carbon cardinal career caress cargo carpenter carpet carriage carrier carrot carve cashier cassette cast castle 
+casual casualty catalog catastrophe cater cathedral Catholic cattle
+cautious cave cease ceiling celebrate celebrity cell cellar cement cemetery census
+cent centigrade centimetre central centre cereal ceremony
+certainty certificate certify chalk chamber champagne chancellor chap
+characteristic charity charm charter chase chef
+chemical cheque cherish cherry chill chimney chin choke chop chorus Christ Christmas chronic
+circuit circular circulate circumference circus cite citizen
+civil civilian clap clanfy clarity clash clasp classification clause claw clay
+clergy clerk cliff climate climax cling
+clip cloak closet
+clothe clue clumsy cluster clutch coach coal coalition
+coarse coast cocaine cock cognitive coherent cohesive
+coil coincide coke collaborate collapse
+collar collide colonel colonial
+comb combination commemorate commence commend comment
+commerce commission commit committee commodity commonplace
+commonwealth  communism commute compact comparable
+compartment compass compassion compatible
+compel compensate compete competent competition competitive
+compile complaint complement complete complex complicate complication compliment
+comply component compose composite composition compound
+comprehend comprise compromise compulsory comrade
+conceal concede conceive concentrate concerning concession concise
+conclude concrete condemn condense conduct conductor confer conference confine
+confirm conflict conform confront confuse
+congress conjunction
+connection conquer conquest
+conscience conscientious conscious consecutive consensus consent
+consequence conservation conservative considerable considerate consist
+consistent console consolidate conspicuous conspiracy constant constituent constitute
+constitution constrain consultant consumption contaminate contemplate contemporary
+contempt contend context continent continual contradict contrary contrast
+contrive controversial convention conventional conversely conversion
+convey convict conviction convince coordinate cop cope copper cord cordial
+corn corporation  correlate correspond
+correspondent corridor corrode corrupt cosmic costume cosy cottage cotton couch cough council counsel counterpart county coupon
+courage court
+courtesy courtyard cousin cow crab crack cradle crane crawl credential creep crew cricket cripple crisis crisp criterion critic critical crop crow crown crucial
+crude cruel cruise crush crust cucumbercue cultivate cunning curb curlosity
+curious curl currency curriculum curse curtain curve cushion custom customary customer
+cyberspace cycle cylinder`
